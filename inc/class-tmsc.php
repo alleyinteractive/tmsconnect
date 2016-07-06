9b9cb7ea763c973225ca77e94010862500e47823
Will Gladstone <will@alleyinteractive.com>
2016-07-06 14:32:40 -0400

    Setup our TMS Connect Zones

--- inc/class-tmsc.php
+++ inc/class-tmsc.php
@@ -5,6 +5,9 @@ if ( ! class_exists( 'TMSC' ) ) {
 
 		private static $instance;
 
+		// The default zones we load objects into
+		public $default_zones = array();
+
 		/**
 		 * Constructor
 		 *
@@ -25,7 +28,74 @@ if ( ! class_exists( 'TMSC' ) ) {
 		}
 
 		public function setup() {
+			/**
+			 * Our Default Zones
+			 */
+			$this->default_zones = array(
+				array(
+					'slug' => 'on-view',
+					'name' => __( 'On View', 'tmsc' ),
+					'description' => __( 'Manages the collection objects that are currently on view', 'tmsc' ),
+				),
+				array(
+					'slug' => 'collection-highlights',
+					'name' => __( 'Collection Highlights', 'tmsc' ),
+					'description' => __( 'Manages the collection objects that are currently highlighted', 'tmsc' ),
+				),
+				array(
+					'slug' => 'new-aquisitions',
+					'name' => __( 'New Aquisitions', 'tmsc' ),
+					'description' => __( 'Manages the collection objects that are new acquisitions, through both gift and purchase', 'tmsc' ),
+				),
+				array(
+					'slug' => 'on-loan',
+					'name' => __( 'On Loan', 'tmsc' ),
+					'description' => __( 'Manages the collection objects that are currently on loan to other museums for temporary exhibitions or long-term display', 'tmsc' ),
+				),
+			);
 
+			// Add zoninator post type support
+			add_action( 'zoninator_post_init', array( $this, 'zoninator_post_type_support' ), 99 );
+			add_filter( 'zoninator_recent_posts_args', array( $this, 'filter_zone_post_types' ) );
+			add_filter( 'zoninator_search_args', array( $this, 'filter_zone_post_types' ) );
+		}
+
+		/**
+		 * Add the Zoninator taxonomy to the custom post types that require it.
+		 * The function calls below just return false if either the taxonomy or post type doesn't exist.
+		 *
+		 * @access public
+		 * @return void
+		 */
+		public function zoninator_post_type_support() {
+
+			$post_types = array( 'tms_object' );
+
+			// Add the Zoninator taxonomy for the defined post types
+			foreach ( $post_types as $post_type ) {
+				add_post_type_support( $post_type, 'zoninator_zones' );
+				register_taxonomy_for_object_type( 'zoninator_zones', $post_type );
+			}
+
+			// The Zoninator internal post_types array has already been written and cached by this point.
+			// We must override it to enabled these post types for the recent posts dropdown and AJAX searches.
+			z_get_zoninator()->post_types = array_merge( z_get_zoninator()->post_types, $post_types );
+		}
+
+		/**
+		 * Only display tmsc objects in our TMSConnect Zones.
+		 */
+		public function filter_zone_post_types( $args ) {
+			$active_zone_id = z_get_zoninator()->_get_request_var( 'zone_id', 0, 'absint' );
+			$tmsc_zones = wp_list_pluck( $this->default_zones, 'slug' );
+			if ( ! empty( $active_zone_id ) ) {
+				$zone = z_get_zone( $active_zone_id );
+				if ( in_array( $zone->slug, $tmsc_zones ) ) {
+					$args['post_type'] = array( 'tms_object' );
+				}
+			}
+
+			return $args;
 		}
 	}
 }
