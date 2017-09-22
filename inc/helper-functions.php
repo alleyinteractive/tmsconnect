3c270d57c69c59d10dfb5a9dbc25ac4c1488bc5a
Kyle Benk <kyle@alleyinteractive.com>
2017-09-22 15:01:02 -0400

    Save progress

--- inc/helper-functions.php
+++ inc/helper-functions.php
@@ -8,11 +8,11 @@
 /**
  * Get an image caption from an attachment.
  *
- * @param  WP_Post|int $attachment The attachment ID or post object.
- * @return string                  The attachment image caption.
+ * @param  int    $attachment The attachment ID.
+ * @return string             The attachment image caption.
  */
-function tmsc_get_image_public_caption( $attachment ) : string {
-	$attachment = get_post( $attachment );
+function tmsc_get_image_public_caption( $attachment_id ) : string {
+	$attachment = get_post( $attachment_id );
 
 	// The public image caption is saved as post content.
 	if ( $attachment instanceof WP_Post ) {
@@ -23,29 +23,22 @@ function tmsc_get_image_public_caption( $attachment ) : string {
 }
 
 /* Element appears on a per-image basis */
-function tms_helper_image_mediaview() {
+function tmsc_helper_image_mediaview() {
 	return '';
 }
 
-/* Element appears on a per-image basis */
-function tms_helper_image_makers() {
-	return '';
-}
-
-/* Element appears on a per-image basis */
-function tms_helper_image_keywords() {
-	return '';
-}
-
-function tms_helper_type() {
+function tmsc_helper_type() {
+	// Takes in post ID and returns term objects in taxonomy classifcation.
 	return '<a href="#">Manuscript</a>';
 }
 
-function tms_helper_maker() {
+function tmsc_helper_maker() {
+	// Takes in post ID and returns term objects in taxonomy constituent.
 	return '<strong>Calligrapher:</strong> <a href="#">Ahmad Sayri</a>';
 }
 
-function tms_helper_time_period() {
+function tmsc_helper_time_period() {
+	// Takes in post ID and returns term objects in taxonomy period.
 	return '<a href="#">Safavid period</a>, <a href="#">1598 (1006 A.H.)</a>';
 }
 
@@ -79,7 +72,7 @@ function tmsc_get_medium( $post_id ) {
 	return get_post_meta( $post_id, 'medium', true );
 }
 
-function tms_helper_style() {
+function tmsc_helper_style() {
 	return '';
 }
 
@@ -93,19 +86,19 @@ function tmsc_get_dimensions( $post_id ) {
 	return get_post_meta( $post_id, 'dimensions', true );
 }
 
-function tms_helper_georgraphy() {
+function tmsc_helper_georgraphy() {
 	return '<a href="#">Iran</a>';
 }
 
-function tms_helper_notes() {
+function tmsc_helper_notes() {
 	return '';
 }
 
-function tms_helper_creditline() {
+function tmsc_helper_creditline() {
 	return 'Purchase &mdash; Charles Lang Freer Endowment';
 }
 
-function tms_helper_collection() {
+function tmsc_helper_collection() {
 	return '<a href="#">Freer Gallery of Art</a>';
 }
 
@@ -119,7 +112,7 @@ function tmsc_get_accession_number( $post_id ) {
 	return get_post_meta( $post_id, 'accession_number', true );
 }
 
-function tms_helper_jades_captions() {
+function tmsc_helper_jades_captions() {
 	return '';
 }
 
@@ -133,11 +126,11 @@ function tmsc_get_location( $post_id ) {
 	return get_post_meta( $post_id, 'location', true );
 }
 
-function tms_helper_classifications() {
+function tmsc_helper_classifications() {
 	return '<a href="#">Manuscript</a>';
 }
 
-function tms_helper_keywords() {
+function tmsc_helper_keywords() {
 	return '<a href="#">illumination</a>, <a href="#">Iran</a>, <a href="#">Islam</a>, <a href="#">muhaqqaq script</a>, <a href="#">naskh script</a>, <a href="#">nasta\'liq script</a>, <a href="#">Qur\'an</a>, <a href="#">Safavid period (1501 - 1722</a>, <a href="#">thuluth script</a>';
 }
 
@@ -151,36 +144,37 @@ function tmsc_get_provenance( $post_id ) {
 	return get_post_meta( $post_id, 'provenance', true );
 }
 
-function tms_helper_constituent() {
-	'<a href="#">Hagop Kevorkian (1872 - 1962)</a>';
+function tmsc_helper_constituent() {
+	// Takes in post ID and returns term objects in taxonomy constituent.
+	return '<a href="#">Hagop Kevorkian (1872 - 1962)</a>';
 }
 
-function tms_helper_description() {
+function tmsc_helper_description() {
 	// Note: when the description exists, part of it is also copied above the slideshow, below the item name
 	return '<p>Manuscript; the Qur\'an with selection of prayers and a falname; Arabic in black naskh script with white headings in illuminated cartouches in thuluth, muhaqqaq, and nasta\'liq script; vocalized in black; 288 folios with 2 shamsa (1 verso, 2 recto) a frontispiece (2 verso, 3 recto), a sarlawh (3 verso), and 3 finispiece (287 verso, 288 recto/verso); inscriptions (fols. 12 recto, 204 recto); rosette verse markers; gold roundels; marginal medallions, inscribed marginal medallions containing the word "ashr" (ten) indicating the end of a tenth verse and the word "juz" (part); standard page: one column; 15 lines of text. Bound folios F1932.66-70 of the same manuscript are accessioned separately.<br>
 		Border: The manuscript is bound in modern red leather over paper-pasteboards.</p>';
 }
 
-function tms_helper_inscriptions() {
+function tmsc_helper_inscriptions() {
 	return '<p>Fol. 12 recto, waqf Ghara Mustafa Pasha.<br>
 			"Endowments of Ghara Mustafa Pasha."<br>
 			Fol. 204 recto, waqf<br>
 			"Inalienable donation."</p>';
 }
 
-function tms_helper_signatures() {
+function tmsc_helper_signatures() {
 	return '';
 }
 
-function tms_helper_markings() {
+function tmsc_helper_markings() {
 	return '';
 }
 
-function tms_helper_label() {
+function tmsc_helper_label() {
 	return '';
 }
 
-function tms_helper_past_label() {
+function tmsc_helper_past_label() {
 	return '<p>1. (Massumeh Farhad, "Arts of the Islamic World", 31 August 2000-5 March 2001)</p>
 
 			<p>Double-page in a Koran<br>
@@ -314,19 +308,19 @@ function tms_helper_past_label() {
 			Freer Gallery of Art, F1932.65, folios 285b–286a</p>';
 }
 
-function tms_helper_jades_curatorial_remarks() {
+function tmsc_helper_jades_curatorial_remarks() {
 	return '';
 }
 
-function tms_helper_unpublished_research() {
+function tmsc_helper_unpublished_research() {
 	return '';
 }
 
-function tms_helper_bibliography() {
+function tmsc_helper_bibliography() {
 	return '';
 }
 
-function tms_helper_rightsstatement() {
+function tmsc_helper_rightsstatement() {
 	return 'Copyright with museum';
 }
 
@@ -341,7 +335,7 @@ function tms_helper_rightsstatement() {
  * true = RTL
  * false = LTR
  */
-function tms_helper_slideshow_rtl() {
+function tmsc_helper_slideshow_rtl() {
 	return true;
 }
 
@@ -349,7 +343,7 @@ function tms_helper_slideshow_rtl() {
 /**
  * Slide to start the slideshow on
  */
-function tms_helper_initial_slide() {
+function tmsc_helper_initial_slide() {
 	return 0;
 }
 
@@ -359,7 +353,7 @@ function tms_helper_initial_slide() {
  *
  * Note: Appears to be stored as a number; I'm not sure what they mean, though, so I stuck with a simple boolean.
  */
-function tms_helper_rights_type() {
+function tmsc_helper_rights_type() {
 	return true;
 }
 
@@ -370,23 +364,23 @@ function tms_helper_rights_type() {
  * Imitating this, which is used to pick what's output for the slideshow: https://github.com/FreerSackler/pattern-library/blob/master/new-templates-from-freer-sackler/object.php#L270-L276
  */
 
-function tms_helper_media_type() {
+function tmsc_helper_media_type() {
 	return 'Image';
 }
 
-function tms_helper_media_view() {
+function tmsc_helper_media_view() {
 	return 'group view';
 }
 
 
 /**
  * Slideshow images - contains an array of the TMS object image IDs and titles.
- * In single-tms_object.php, it's looped through to recreate the original markup from the Object-Template example
+ * In single-tmsc_object.php, it's looped through to recreate the original markup from the Object-Template example
  *
  * To emulate the Object-Template example, some images have a 'data-media-master-id' value
  */
-function tms_helper_images() {
-	$tms_images = array(
+function tmsc_helper_images() {
+	$tmsc_images = array(
 		0 => array(
 			'id'                   => 'F1932.65_folio562',
 			'filename'             => 'filename1',
@@ -437,5 +431,5 @@ function tms_helper_images() {
 			'media-master-id'      => '',
 		),
 	);
-	return $tms_images;
+	return $tmsc_images;
 }
