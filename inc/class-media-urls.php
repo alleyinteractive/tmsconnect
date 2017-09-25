1d09f2c6f639a200c32629a003b7f800d7e7a9af
Kyle Benk <kyle@alleyinteractive.com>
2017-09-25 08:42:42 -0400

    Add support for custom image source

--- inc/class-media-urls.php
+++ inc/class-media-urls.php
@@ -26,14 +26,29 @@ class Media_URLs {
 	/**
 	 * Filter the media source URL.
 	 *
-	 * @param array|false  $image         Either array with src, width & height, icon src, or false.
-	 * @param int          $attachment_id Image attachment ID.
-	 * @param string|array $size          Size of image. Image size or array of width and height values
-	 *                                    (in that order). Default 'thumbnail'.
-	 * @param bool         $icon          Whether the image should be treated as an icon. Default false.
-	 * @return [type]                [description]
+	 * @param  array|false  $image         Either array with src, width & height, icon src, or false.
+	 * @param  int          $attachment_id Image attachment ID.
+	 * @param  string|array $size          Size of image. Image size or array of width and height values
+	 *                                     (in that order). Default 'thumbnail'.
+	 * @param  bool         $icon          Whether the image should be treated as an icon. Default false.
+	 * @return array|false  $image         Either array with src, width & height, icon src, or false.
 	 */
 	public function filter_image_source( $image, $attachment_id, $size, $icon ) {
+		// See if this attachment has a filename attribute.
+		$filename = get_post_meta( $attachment_id, 'tms_media_filename', true );
+		if ( ! empty( $filename ) ) {
+			// Get the Image Delivery System URL.
+			$image_delivery_system_url = get_option( 'tmsc-image-url' );
+
+			// Create the new image source URL.
+			$image_src = $image_delivery_system_url . '/' . $filename;
+
+			// Add the new image src to the existing image.
+			if ( is_array( $image ) && ! empty( $image[0] ) && is_string( $image[0] ) ) {
+				$image[0] = $image_src;
+			}
+		}
+
 		return $image;
 	}
 }
