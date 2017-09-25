d0c67862d6761957dedcb4059a5455e968e5a82a
Kyle Benk <kyle@alleyinteractive.com>
2017-09-25 12:05:13 -0400

    Add linked object helper funciton

--- inc/helper-functions.php
+++ inc/helper-functions.php
@@ -50,7 +50,7 @@ function tmsc_get_term_meta( int $term_id, string $meta_key ) {
  *
  * @param  int    $post_id  The post ID.
  * @param  mixed  $taxonomy The taxonomies to get the terms from.
- * @return mixed            The object terms.
+ * @return array            An array of WP_Term objects.
  */
 function tmsc_get_object_terms( int $post_id, $taxonomy ) {
 	$terms = wp_get_object_terms( $post_id, $taxonomy );
@@ -64,6 +64,62 @@ function tmsc_get_object_terms( int $post_id, $taxonomy ) {
 	 */
 	return apply_filters( 'tmsc_get_object_terms', $terms, $post_id, $taxonomy );
 }
+
+/**
+ * Get a post's classifcations.
+ *
+ * @param  int    $post_id  The post ID.
+ * @param  mixed  $taxonomy The taxonomies to get the terms from.
+ * @return array            An array of WP_Post objects.
+ */
+function tmsc_get_linked_objects( int $post_id, $taxonomy ) {
+	$terms = wp_get_object_terms( $post_id, $taxonomy );
+	$cache_key = 'tmsc_linked_objects_' . md5( $post_id . $taxonomy );
+	$linked_objects = get_transient( $cache_key );
+
+	if ( false === $linked_objects ) {
+		$linked_objects = [];
+
+		// Loop through all terms and get the linked post.
+		foreach ( $terms as $term ) {
+			if ( ! empty( $term->term_id ) ) {
+				$linked_post = tmsc_get_linked_post( $term->term_id );
+
+				// Add it to the linked objects.
+				if ( $linked_post instanceof WP_Post ) {
+					$linked_objects[] = $linked_post;
+				}
+			}
+		}
+
+		set_transient( $cache_key, $linked_objects, DAY_IN_SECONDS );
+	}
+
+	/**
+	 * Filter the linked objects.
+	 *
+	 * @param mixed  $linked_terms The linked objects.
+	 * @param int    $post_id      The post ID.
+	 * @param mixed  $taxonomy     The taxonomies to get the terms from.
+	 */
+	return apply_filters( 'tmsc_get_linked_objects', $linked_objects, $post_id, $taxonomy );
+}
+
+/**
+ * Clear the `tmsc_get_linked_objects` cache on post save.
+ *
+ * @param  int $post_id The post ID.
+ */
+function tmsc_get_linked_objects_clear_cache( $post_id ) {
+	// Get all of the post taxonomies.
+	$taxonomy_names = get_post_taxonomies( $post_id );
+
+	// Delete the transient for each taxonomy on the post.
+	foreach ( $taxonomy_names as $taxonomy_name ) {
+		delete_transient( 'tmsc_linked_objects_' . md5( $post_id . $taxonomy_name ) );
+	}
+}
+add_action( 'save_post', 'tmsc_get_linked_objects_clear_cache' );
 
 /**
  * Get an image caption from an attachment.
