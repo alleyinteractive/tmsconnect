523848f428cb3ee994ff219962d42123260c8972
Will Gladstone <will@alleyinteractive.com>
2017-10-04 16:33:33 -0400

    remove creds

--- inc/class-search.php
+++ inc/class-search.php
@@ -37,8 +37,8 @@ class Search {
 	protected function setup_auth() {
 		// If ES hosted by Found and protected by Shield, it requires http basic
 		// authentication.
-		$username = apply_filter( 'tmsc_sp_username', '' );
-		$password = apply_filter( 'tmsc_sp_pass', '' );
+		$username = apply_filters( 'tmsc_sp_username', '' );
+		$password = apply_filters( 'tmsc_sp_pass', '' );
 		if ( function_exists( 'SP_API' ) && false !== strpos( SP_Config()->get_setting( 'host' ), 'aws.found.io' ) ) {
 			SP_API()->request_defaults['headers']['Authorization'] = 'Basic ' . base64_encode( "$username:$password" );
 		}
